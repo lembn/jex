eab1327c9e1a59d67e8f8231a95ec9961659ef3a
Lem <61047330+lembn@users.noreply.github.com>
2022-02-08 22:53:18 +0000

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import pkg.MyClass;
-
-public class Main {
-    public static void main(String[] args) {
-        MyClass mc = new MyClass();
-        System.out.println("Hello World!");
-        System.out.println("Hello World");
-    }
-}
