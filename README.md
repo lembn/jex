4823034d00b6543fb48517486cf35950822e834e
lembn <lembn@outlook.com>
2022-02-09 12:10:50 +0000

    we need to finish the code in setup() to setup the config file

--- README.md
+++ README.md
@@ -26,5 +26,16 @@ These attributes can also be set in the tool directly, by passing them as comman
 
 - [ ] Add support for command line args as well as config file (`jexe.json`)
   - [ ] Pass path to config file as argument, default value is root
+- [ ] Better logging
+  - [ ] Format
+  - [ ] Log to file
+- [ ] Handle errors
+  - [ ] Log compile-time errors, then quit (don't run)
+  - [ ] Log run-time errors
+  - [ ] Create flag that will show errors
+  - [ ] Reset errors per build
+- [ ] Debug mode
+  - [ ] Call `jdb` instead of `java`
+  - [ ] Debug binaries have to be compiled differently
 - [ ] Create executable
   - [ ] Create release
--- main.py
+++ main.py
@@ -2,72 +2,141 @@ import json
 import subprocess
 import os
 import hashlib
+import click
 
 conv = lambda x: x.replace("\\", "/")
 join = lambda x, y: conv(os.path.join(x, y))
 
-BUF_SIZE = 65536 # 64Kb
-BUILD = "./build"
-META = join(BUILD, ".jexe")
-SOURCES = "./src"
-ENTRY = "Main"
-CONFIG = "./jexe.json"
-CLASSES = join(META, "classes")
-HASHES = join(META, "classes.json")
+BUF_SIZE = 65536  # 64Kb
 
-print("Preparing to build...")
-if os.path.isfile(CONFIG):
-    print("Found custom build configuration")
-    with open(CONFIG, "r") as metafile:
-        metadata = json.load(metafile)
-        BUILD = conv(metadata["build"])
-        META = join(BUILD, ".jexe")
-        SOURCES = conv(metadata["sources"])
-        ENTRY = conv(metadata["entry"])
-else:
-    print("Using default build configuration.")
 
-if not os.path.exists(META):
-    os.makedirs(META)
-if not os.path.exists(HASHES):
-    with open(HASHES, "w") as hash_file:
-        hash_file.write("{}")
+def setup(path: str, config: dict):
+    print("Preparing to build...")
+    if os.path.isfile(path):
+        print("Found custom build configuration")
+        with open(path, "r") as config_file:
+            metadata = json.load(config_file)
+            BUILD = conv(metadata["build"])
+            SOURCES = conv(metadata["sources"])
+            ENTRY = conv(metadata["entry"])
+    else:
+        print("Using default build configuration.")
 
-print("Collecting source files")
-recompile = False
-with open(HASHES, "r+") as hash_file:
-    hashes = json.load(hash_file)
-    filenames = []
 
-    with open(CLASSES, "w") as classes_file:
-        for root, _, files in os.walk(SOURCES):
-            for name in files:
-                if ".java" in name:
-                    name = join(root, name)
-                    filenames.append(name)
-                    md5 = hashlib.md5()
-                    with open(name, 'rb') as f:
-                        while True:
-                            data = f.read(BUF_SIZE)
-                            if not data:
-                                break
-                            md5.update(data)
-                    file_hash = md5.hexdigest()
-                    if name in hashes and hashes[name] == file_hash:
-                        continue
-                    print(f"Found updated file: {name}")
-                    recompile = True
-                    classes_file.write(f"{name}\n")
-                    hashes[name] = file_hash
+def prepare():
+    if not os.path.exists(META):
+        os.makedirs(META)
+    if not os.path.exists(HASHES):
+        with open(HASHES, "w") as hash_file:
+            hash_file.write("{}")
 
-    hashes = {i: j for i, j in hashes.items() if i in filenames}
-    hash_file.seek(0)
-    hash_file.truncate()
-    hash_file.write(json.dumps(hashes))
+    print("Collecting source files")
+    recompile = False
+    with open(HASHES, "r+") as hash_file:
+        hashes = json.load(hash_file)
+        filenames = []
 
-if recompile:
-    print("Compiling...")
-    subprocess.run(["javac", "-classpath", BUILD, f"@{CLASSES}", "-d", BUILD])
+        with open(CLASSES, "w") as classes_file:
+            for root, _, files in os.walk(SOURCES):
+                for name in files:
+                    if ".java" in name:
+                        name = join(root, name)
+                        filenames.append(name)
+                        md5 = hashlib.md5()
+                        with open(name, "rb") as f:
+                            while True:
+                                data = f.read(BUF_SIZE)
+                                if not data:
+                                    break
+                                md5.update(data)
+                        file_hash = md5.hexdigest()
+                        if name in hashes and hashes[name] == file_hash:
+                            continue
+                        print(f"Found updated file: {name}")
+                        recompile = True
+                        classes_file.write(f"{name}\n")
+                        hashes[name] = file_hash
 
-print(f"Running from: {ENTRY}")
-subprocess.run(["java", "-cp", BUILD, ENTRY])
+        hashes = {i: j for i, j in hashes.items() if i in filenames}
+        hash_file.seek(0)
+        hash_file.truncate()
+        hash_file.write(json.dumps(hashes))
+
+
+def run(compile: bool, run: bool):
+    if compile:
+        print("Compiling...")
+        subprocess.run(["javac", "-classpath", BUILD, f"@{CLASSES}", "-d", BUILD])
+
+    if run:
+        print(f"Running from: {ENTRY}")
+        subprocess.run(["java", "-cp", BUILD, ENTRY])
+
+
+@click.command()
+@click.option(
+    "-c",
+    "--config",
+    default="./jexe.json",
+    type=click.Path(dir_okay=False),
+    help="Path to the 'jexe.json' configuration file.",
+)
+@click.option(
+    "-b",
+    "--build",
+    default="./build",
+    type=click.Path(file_okay=False),
+    help="Directory path to compile to.",
+)
+@click.option(
+    "-s",
+    "--sources",
+    default="./src",
+    type=click.Path(file_okay=False),
+    help="Path to the containing directory of the source code.",
+)
+@click.option(
+    "-e",
+    "--entry",
+    default="Main",
+    type=click.STRING,
+    help="Java FQN of the entry point file.",
+)
+@click.option(
+    "-d", "--debug", default=False, type=click.BOOL, help="Run in debug mode."
+)
+@click.option(
+    "--compile/--no-compile",
+    default=False,
+    type=click.BOOL,
+    help="Compile the project.",
+)
+@click.option("--run/--no-run", default=False, type=click.BOOL, help="Run the project.")
+@click.option("--errors", default=False, type=click.BOOL, help="Show last errors.")
+def main(
+    config: str,
+    build: str,
+    sources: str,
+    entry: str,
+    debug: bool,
+    compile: bool,
+    run: bool,
+    errors: bool,
+):
+    """
+    A lightweight wrapper for the Java CLI programs to act as a build tool
+    for fast development of complex projects in a simple environment.
+    """
+
+    META = join(build, ".jexe")
+    CLASSES = join(META, "classes")
+    HASHES = join(META, "classes.json")
+
+    # TODO: check config opt actually points to a valid jexe.json
+    setup(config)
+    prepare()
+    run()
+
+
+if __name__ == "__main__":
+    main(prog_name="jexe")
